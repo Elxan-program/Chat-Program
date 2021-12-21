51ca20b43276b69a03a9513f75930b6336076c84
Atay_qn28 <Atay_qn28@STHQ0118-08>
2021-12-21 11:59:41 +0400

    Add project files.

---
+++ Chat Program/Entities/SenderAndReciever.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Program.Entities
+{
+	public class SenderAndReciever
+	{
+		public static void Send()
+		{
+			var soket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			var ip = IPAddress.Parse("");//IPADDRESS
+			var port = 27001;
+			var ep = new IPEndPoint(ip, port);
+		}
+	}
+}
